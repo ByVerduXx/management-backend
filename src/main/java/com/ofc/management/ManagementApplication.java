9541aaf366497d92cd2fe97e474bfc89faaab440
Daniel Verduras Gallego <daniel.verduras21@gmail.com>
2023-02-04 03:32:02 +0100

    Basic configs

--- src/main/java/com/ofc/management/ManagementApplication.java
+++ src/main/java/com/ofc/management/ManagementApplication.java
@@ -2,8 +2,9 @@ package com.ofc.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { HibernateJpaAutoConfiguration.class })
 public class ManagementApplication {
 
 	public static void main(String[] args) {
